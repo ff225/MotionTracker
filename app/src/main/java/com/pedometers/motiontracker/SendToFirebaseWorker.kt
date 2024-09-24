c63ef821deb692a831afb0dacfd85c3505ddac75
Francesco Franco <frafranco16@gmail.com>
2024-09-24 16:25:32 +0200

    Add SendToFirebaseWorker class for uploading files to Firebase storage

---
+++ app/src/main/java/com/pedometers/motiontracker/SendToFirebaseWorker.kt
@@ -0,0 +1,29 @@
+package com.pedometers.motiontracker
+
+import android.content.Context
+import android.util.Log
+import androidx.core.net.toUri
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.google.firebase.Firebase
+import com.google.firebase.storage.storage
+import java.io.File
+
+class SendToFirebaseWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+    private val storage = Firebase.storage.reference
+
+    override suspend fun doWork(): Result {
+        val path = inputData.getString("file")?.toUri()
+        // get file from directory
+        val file = path?.let { File(it.path) }
+        // upload file to firebase
+        storage.child(file!!.name).putFile(file.toUri())
+            .addOnSuccessListener {
+                Log.d("SendToFirebaseWorker", "File uploaded successfully")
+            }
+            .addOnFailureListener {
+                Log.e("SendToFirebaseWorker", "Error uploading file", it)
+            }
+        return Result.success()
+    }
+}
