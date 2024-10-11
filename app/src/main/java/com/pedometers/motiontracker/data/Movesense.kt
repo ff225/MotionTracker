ce903a525bc24c40ec02880ee450c50c268933b4
Francesco Franco <frafranco16@gmail.com>
2024-10-11 17:55:08 +0200

    add movesense object

---
+++ app/src/main/java/com/pedometers/motiontracker/data/Movesense.kt
@@ -0,0 +1,45 @@
+package com.pedometers.motiontracker.data
+
+import android.util.Log
+
+object Movesense {
+
+    var name: String? = null
+        private set
+    var macAddr: String? = null
+        private set
+
+    var isConnected = false
+
+    var position: PositionWearable? = null
+        private set
+
+    var frequencyHertz: FrequencyHertz = FrequencyHertz.HZ_13
+        private set
+
+    fun init(name: String, macAddr: String, isConnected: Boolean = false) {
+        Log.d("Movesense", "init: $name, $macAddr")
+        this.name = name
+        this.macAddr = macAddr
+        this.isConnected = isConnected
+    }
+
+
+    fun setFrequency(frequencyHertz: FrequencyHertz) {
+        Log.d("Movesense", "setFrequency: $frequencyHertz")
+        this.frequencyHertz = frequencyHertz
+    }
+
+    fun setPosition(position: PositionWearable) {
+        Log.d("Movesense", "setPosition: $position")
+        this.position = position
+    }
+
+    fun reset() {
+        Log.d("Movesense", "reset")
+        Log.d("Movesense", "value: $name, $macAddr")
+        name = null
+        macAddr = null
+        isConnected = false
+    }
+}
