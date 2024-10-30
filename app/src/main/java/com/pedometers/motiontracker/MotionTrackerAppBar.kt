9ba726ee1ca1283da8281847f11daae6c9d5e480
Francesco Franco <frafranco16@gmail.com>
2024-10-30 17:51:54 +0100

    add new iconbutton

--- app/src/main/java/com/pedometers/motiontracker/MotionTrackerAppBar.kt
+++ app/src/main/java/com/pedometers/motiontracker/MotionTrackerAppBar.kt
@@ -1,13 +1,16 @@
 package com.pedometers.motiontracker
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Build
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,21 +24,38 @@ fun MotionTrackerAppBar(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {},
+    navigateToInfo: () -> Unit = {},
+    navigateToUpdateID: () -> Unit = {},
     navigateToMovesense: () -> Unit = {}
 
 ) {
-    CenterAlignedTopAppBar(
+    TopAppBar(
         title = { Text(title) },
         modifier = modifier,
         scrollBehavior = scrollBehavior,
         actions = {
             if (!canNavigateBack)
-                IconButton(onClick = navigateToMovesense) {
-                    Icon(
-                        imageVector = Icons.Filled.Build,
-                        contentDescription = null
-                    )
+                Row {
+                    IconButton(onClick = navigateToInfo) {
+                        Icon(
+                            imageVector = Icons.Filled.Info,
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(onClick = navigateToUpdateID) {
+                        Icon(
+                            imageVector = Icons.Filled.Warning,
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(onClick = navigateToMovesense) {
+                        Icon(
+                            imageVector = Icons.Filled.Build,
+                            contentDescription = null
+                        )
+                    }
                 }
+
         },
         navigationIcon = {
             if (canNavigateBack)
